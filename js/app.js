45f158bacdd7571260d52ad01a430d0a9fb12afb
Justin Co <jcoen5209@gmail.com>
2016-12-17 11:33:25 -0500

    Added timeago.js package for formatting dates | Fixed Messages CSS

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ const app = express();
 const Twitter = require('twitter');
 const twitterTimeline = require('./twitterData.js').twitterTimeline;
 const twitterFriends = require('./twitterData.js').twitterFriends;
+const messagesRecieved = require('./twitterData.js').messagesRecieved;
+const messagesSent = require('./twitterData.js').messagesSent;
+
+let timelineResults;
+let friendResults;
+let messagesResults;
+let twitterData;
 
 app.set('views', './templates');
 app.set('view engine', 'jade');
@@ -11,10 +18,23 @@ app.use(express.static('./css'));
 app.use(express.static('./images'));
 
 twitterTimeline()
+.then((results) => {timelineResults = results})
 .then(twitterFriends)
-.then((data) => {
+.then((results) => {friendResults = Object.assign(timelineResults, results)})
+.then(messagesRecieved)
+.then((results) => {messagesResults = Object.assign(friendResults, results)})
+.then(messagesSent)
+.then((results) => {twitterData = Object.assign(messagesResults, results)})
+.then(() => {
+	//console.log(twitterData)
 	app.get('/', (req, res) => {
-		res.render('layout', {tweets: data.tweets, friends: data.friends});
+		res.render('layout', {
+			data: twitterData, 
+			tweets: twitterData.tweets, 
+			friends: twitterData.friends, 
+			messagesRecieved: twitterData.messagesRecieved,
+			messagesSent: twitterData.messagesSent
+		});
 	})
 });
 
--- js/twitterData.js
+++ js/twitterData.js
@@ -1,5 +1,7 @@
+'use strict';
 const Twitter = require('twitter');
 const format = require('dateformat');
+const timeago = require('timeago.js');
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -8,7 +10,7 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const params = {screen_name: 'joerogan'};
+const params = {screen_name: '_jtco'};
 
 const twitterTimeline = () => {
 	const promise = new Promise((resolve, reject) => {
@@ -18,11 +20,13 @@ const twitterTimeline = () => {
 		    	name: tweets[0].user.name,
 		    	screen_name: tweets[0].user.screen_name,
 		    	profile_img: tweets[0].user.profile_image_url,
-		    	tweet1: {text: tweets[0].text, date: tweetTime(tweets[0].created_at), favorite_count: tweets[0].favorite_count, retweet_count: tweets[0].retweet_count}, 
-		    	tweet2: {text: tweets[1].text, date: tweetTime(tweets[1].created_at), favorite_count: tweets[1].favorite_count, retweet_count: tweets[1].retweet_count},
-		    	tweet3: {text: tweets[2].text, date: tweetTime(tweets[2].created_at), favorite_count: tweets[2].favorite_count, retweet_count: tweets[2].retweet_count},
-		    	tweet4: {text: tweets[3].text, date: tweetTime(tweets[3].created_at), favorite_count: tweets[3].favorite_count, retweet_count: tweets[3].retweet_count},
-		    	tweet5: {text: tweets[4].text, date: tweetTime(tweets[4].created_at), favorite_count: tweets[4].favorite_count, retweet_count: tweets[4].retweet_count}
+		    	tweets: {
+			    	tweet1: {text: tweets[0].text, date: new timeago().format(tweets[0].created_at), favorite_count: tweets[0].favorite_count, retweet_count: tweets[0].retweet_count}, 
+			    	tweet2: {text: tweets[1].text, date: new timeago().format(tweets[1].created_at), favorite_count: tweets[1].favorite_count, retweet_count: tweets[1].retweet_count},
+			    	tweet3: {text: tweets[2].text, date: new timeago().format(tweets[2].created_at), favorite_count: tweets[2].favorite_count, retweet_count: tweets[2].retweet_count},
+			    	tweet4: {text: tweets[3].text, date: new timeago().format(tweets[3].created_at), favorite_count: tweets[3].favorite_count, retweet_count: tweets[3].retweet_count},
+			    	tweet5: {text: tweets[4].text, date: new timeago().format(tweets[4].created_at), favorite_count: tweets[4].favorite_count, retweet_count: tweets[4].retweet_count}
+			  	}
 		    })
 		  }
 		});	
@@ -35,13 +39,30 @@ const twitterFriends = (tweets) => {
 		client.get('friends/list', params, function(error, friends, response) {
 		  if (!error) {
 		  	resolve({
-		  		tweets: tweets,
 		  		friends: {
 			  		friend1: {name: friends.users[0].name, screen_name: friends.users[0].screen_name, profile_img: friends.users[0].profile_image_url},
 			  		friend2: {name: friends.users[1].name, screen_name: friends.users[1].screen_name, profile_img: friends.users[1].profile_image_url},
 			  		friend3: {name: friends.users[2].name, screen_name: friends.users[2].screen_name, profile_img: friends.users[2].profile_image_url},
 			  		friend4: {name: friends.users[3].name, screen_name: friends.users[3].screen_name, profile_img: friends.users[3].profile_image_url},
 			  		friend5: {name: friends.users[4].name, screen_name: friends.users[4].screen_name, profile_img: friends.users[4].profile_image_url}
+			  	}
+		  	})
+		  }
+		});	
+	})
+	return promise
+}
+
+const messagesRecieved = (friends) => {
+	const promise = new Promise((resolve, reject) => {
+		client.get('direct_messages.json?count=3', params, function(error, messages, response) {
+		  if (!error) {
+		  	//console.log(messages[0])
+		  	resolve({
+		  		messagesRecieved: {
+			  		message1: {msg: messages[0].text, date: new timeago().format(messages[0].created_at), profile_img: messages[0].sender.profile_image_url},
+			  		message2: {msg: messages[1].text, date: new timeago().format(messages[1].created_at), profile_img: messages[1].sender.profile_image_url},
+			  		message3: {msg: messages[2].text, date: new timeago().format(messages[2].created_at), profile_img: messages[2].sender.profile_image_url}
 		  		}
 		  	})
 		  }
@@ -50,21 +71,25 @@ const twitterFriends = (tweets) => {
 	return promise
 }
 
-const tweetTime = (created_at) => {
-	const now = parseInt(format(new Date(), 'HH', true));
-	const timeTweeted = parseInt(created_at.split(" ")[3]);
-	let dateTweeted = created_at.split(" ");
-	dateTweeted.splice(3);
-	dateTweeted = dateTweeted[0] + ' / ' + dateTweeted[1] + ' / ' + dateTweeted[2]; 
-	if(now - timeTweeted >= 1) {
-		return (now - timeTweeted) + 'h';
-	} else if(now - timeTweeted === 0){
-		return '>1h';
-	} else {
-		return dateTweeted
-	}
+const messagesSent = (messagesRecieved) => {
+	const promise = new Promise((resolve, reject) => {
+		client.get('direct_messages/sent.json?count=3', params, function(error, messages, response) {
+		  if (!error) {
+		  	resolve({
+		  		messagesSent: {
+			  		message1: {msg: messages[0].text, date: new timeago().format(messages[0].created_at), profile_img: messages[0].sender.profile_image_url},
+			  		message2: {msg: messages[1].text, date: new timeago().format(messages[1].created_at), profile_img: messages[1].sender.profile_image_url},
+			  		message3: {msg: messages[2].text, date: new timeago().format(messages[2].created_at), profile_img: messages[2].sender.profile_image_url}
+		  		}
+		  	})
+		  }
+		});	
+	})
+	return promise
 }
 
 module.exports.twitterTimeline = twitterTimeline;
 module.exports.twitterFriends = twitterFriends;
+module.exports.messagesRecieved = messagesRecieved;
+module.exports.messagesSent = messagesSent;
 
