acec36ab5db73a1bdf04f0d8660b39bedbaaf8a4
Justin Co <jcoen5209@gmail.com>
2017-01-02 16:30:42 -0500

    Commented twitterData.js

--- js/twitterData.js
+++ js/twitterData.js
@@ -3,6 +3,7 @@ const Twitter = require('twitter');
 const format = require('dateformat');
 const timeago = require('timeago.js');
 
+// Credentials are entered in process environment - this ensures the security of sensitive data
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -10,16 +11,22 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+// The screen_name which will be used with the Twitter package
 const params = {screen_name: '_jtco'};
 
+// GET user's latest 5 tweets
 const twitterTimeline = () => {
+	// Create a new promise to resolve & return a GET request for the user's timeline
 	const promise = new Promise((resolve, reject) => {
+		// Initial API call for user's timeline
 		client.get('statuses/user_timeline', params, (error, tweets, response) => {
 		  if (!error) {
+		  	// Once data has been returned, appoint data to properties - username; screen_name; profile_img; tweets
 		    resolve({
 		    	name: tweets[0].user.name,
 		    	screen_name: tweets[0].user.screen_name,
 		    	profile_img: tweets[0].user.profile_image_url,
+		    	// Appoint data for each tweet - text; date ( Format time by using timeago's package ); favorite_count; retweet_count
 		    	tweets: {
 			    	tweet1: {text: tweets[0].text, date: new timeago().format(tweets[0].created_at), favorite_count: tweets[0].favorite_count, retweet_count: tweets[0].retweet_count}, 
 			    	tweet2: {text: tweets[1].text, date: new timeago().format(tweets[1].created_at), favorite_count: tweets[1].favorite_count, retweet_count: tweets[1].retweet_count},
@@ -34,10 +41,14 @@ const twitterTimeline = () => {
 	return promise
 }
 
+// GET user's latest 5 friends - twitterTimeline's resolved promise is passed into here in order to build upon the initial object
 const twitterFriends = (tweets) => {
+	// Create a new promise to resolve & return a GET request for the user's latest friends
 	const promise = new Promise((resolve, reject) => {
+		// API call for friends list
 		client.get('friends/list', params, function(error, friends, response) {
 		  if (!error) {
+		  	// Once data has been returned, appoint data to properties, within the friends object - friend's name; friend's screen_name; friend's profile_img
 		  	resolve({
 		  		friends: {
 			  		friend1: {name: friends.users[0].name, screen_name: friends.users[0].screen_name, profile_img: friends.users[0].profile_image_url},
@@ -53,11 +64,14 @@ const twitterFriends = (tweets) => {
 	return promise
 }
 
+// GET user's latest 3 messages recieved - twitterFriends's resolved promise is passed into here in order to build upon the initial object
 const messagesRecieved = (friends) => {
+	// Create a new promise to resolve & return a GET request for the user's latest 3 messages recieved
 	const promise = new Promise((resolve, reject) => {
+		// API call for messages recieved
 		client.get('direct_messages.json?count=3', params, function(error, messages, response) {
 		  if (!error) {
-		  	//console.log(messages[0])
+		  	// Once data has been returned, appoint data to propterties, within the messagesRecieved object- message text; date ( Format time by using timeago's package ); sender's profile_img
 		  	resolve({
 		  		messagesRecieved: {
 			  		message1: {msg: messages[0].text, date: new timeago().format(messages[0].created_at), profile_img: messages[0].sender.profile_image_url},
@@ -71,10 +85,14 @@ const messagesRecieved = (friends) => {
 	return promise
 }
 
+// GET user's latest 3 messages sent - messagesRecieved's resolved promise is passed into here in order to build upon the initial object
 const messagesSent = (messagesRecieved) => {
+	// Create a new promise to resolve & return a GET request for the user's latest 3 messages sent
 	const promise = new Promise((resolve, reject) => {
+		// API call for messages sent
 		client.get('direct_messages/sent.json?count=3', params, function(error, messages, response) {
 		  if (!error) {
+		  	// Once data has been returned, appoint data to propterties, within the messagesSent object- message text; date ( Format time by using timeago's package ); sender's profile_img
 		  	resolve({
 		  		messagesSent: {
 			  		message1: {msg: messages[0].text, date: new timeago().format(messages[0].created_at), profile_img: messages[0].sender.profile_image_url},
@@ -87,7 +105,7 @@ const messagesSent = (messagesRecieved) => {
 	})
 	return promise
 }
-
+// Export Modules to be used in app.js
 module.exports.twitterTimeline = twitterTimeline;
 module.exports.twitterFriends = twitterFriends;
 module.exports.messagesRecieved = messagesRecieved;
